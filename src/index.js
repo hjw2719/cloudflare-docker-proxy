54ca21d198806922b5a21e3cef9fa64b4169787c
hjw2719 <barry.hjw@gmail.com>
2024-07-29 21:02:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.myhjwlzf.top": "https://registry-1.docker.io",
+  "quay.myhjwlzf.top": "https://quay.io",
+  "gcr.myhjwlzf.top": "https://gcr.io",
+  "k8s-gcr.myhjwlzf.top": "https://k8s.gcr.io",
+  "k8s.myhjwlzf.top": "https://registry.k8s.io",
+  "ghcr.myhjwlzf.top": "https://ghcr.io",
+  "cloudsmith.myhjwlzf.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
